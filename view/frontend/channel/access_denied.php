c86bdec0ab5639464c9073deb0bbffae3031b3b7
CreativeMindsSolutions <CreativeMindsSolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-02 11:36:01 +0000

    replace to version 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/cm-video-lesson-manager/trunk@1126228 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- view/frontend/channel/access_denied.php
+++
@@ -1,4 +0,0 @@
-<div class="cmvl-info-block cmvl-access-denied">
-	<p><?php echo com\cminds\videolessons\model\Labels::getLocalized('msg_access_denied'); ?></p>
-	<?php do_action('cmvl_access_denied_content', $channel); ?>
-</div>

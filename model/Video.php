80e4f6fe972bb90c91aa195acc8e21b3c94a6644
CreativeMindsSolutions <CreativeMindsSolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-08 15:50:16 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/cm-video-lesson-manager/trunk@1156286 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ model/Video.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace com\cminds\videolesson\model;
+
+class Video extends Model {
+	
+	const META_POST_VIDEO_DURATION = 'cmvl_video_duration';
+	
+	protected $video;
+	protected $channel;
+	protected $searchScore;
+	
+	
+	function __construct($video, Channel $channel) {
+		
+		$this->video = $video;
+		$this->channel = $channel;
+		
+		$this->cacheDuration();
+		
+	}
+		
+		
+	protected function cacheDuration() {
+		$channelId = $this->getChannel()->getId();
+		$metaKey = self::META_POST_VIDEO_DURATION .'_'. $this->getId();
+		if (!get_post_meta($channelId, $metaKey, $single = true)) {
+			update_post_meta($channelId, $metaKey, $this->getDuration());
+		}
+		return $this;
+	}
+	
+	
+	function getTitle() {
+		return $this->video['name'];
+	}
+	
+	
+	function getDescription() {
+		return $this->video['description'];
+	}
+	
+	
+	function getDuration() {
+		return $this->video['duration'];
+	}
+	
+	
+	function getDurationFormatted() {
+		$duration = $this->getDuration();
+		if ($duration > 3600) return Date('H:i:s', $duration);
+		else return Date('i:s', $duration);
+	}
+	
+	
+	function getThumbUri($minWidth = 100) {
+		if (!empty($this->video['pictures']['sizes'])) {
+			foreach ($this->video['pictures']['sizes'] as $picture) {
+				if ($picture['width'] >= $minWidth) {
+					return $picture['link'];
+				}
+			}
+		}
+	}
+	
+	
+	function getScreenshot() {
+		if (!empty($this->video['pictures']['sizes'])) {
+			$picture = end($this->video['pictures']['sizes']);
+			return $picture['link'];
+		}
+	}
+	
+	
+	function getChannel() {
+		return $this->channel;
+	}
+	
+	
+	function getPermalink() {
+		return add_query_arg('video', urlencode($this->getId()), $this->getChannel()->getPermalink());
+	}
+	
+	
+	function getPlayerUrl(array $options = array()) {
+		$options = shortcode_atts(array(
+			'api' => 1,
+			'autoplay' => 0,
+			'badge' => 0,
+			'byline' => 0,
+			'portrait' => 0,
+			'title' => 0,
+			'player_id' => null,
+		), $options);
+		return add_query_arg(urlencode_deep($options), '//player.vimeo.com/video/' . urlencode($this->getId()));
+	}
+	
+	
+	function getPlayer(array $options = array()) {
+		$this->unlock();
+		if (empty($options['player_id'])) $options['player_id'] = 'cmvl-player-' . rand(0, 99999);
+		return '<iframe id="'. esc_attr($options['player_id']) .'" src="'. esc_attr($this->getPlayerUrl($options))
+			. '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+	}
+	
+	
+	function getId() {
+		return preg_replace('/[^0-9]/', '', $this->video['uri']);
+	}
+	
+	
+	function getVimeoUri() {
+		return '/videos/'. $this->getId();
+	}
+	
+	
+	function getPrivacyView() {
+		return $this->video['privacy']['view'];
+	}
+	
+	
+	function setPrivacyView($value) {
+		Vimeo::getInstance()->request($this->getVimeoUri(), array('privacy' => array('view' => $value)), 'PATCH');
+		return $this;
+	}
+	
+	
+	function getPrivacyEmbed() {
+		return $this->video['privacy']['embed'];
+	}
+	
+	
+	function setPrivacyEmbed($value) {
+		Vimeo::getInstance()->request($this->getVimeoUri(), array('privacy' => array('embed' => $value)), 'PATCH');
+		return $this;
+	}
+	
+	
+	function getPrivacyDomains() {
+		$results = array();
+		$domains = Vimeo::getInstance()->request($this->getVimeoUri() . '/privacy/domains');
+		if (!empty($domains['body']['data'])) foreach ($domains['body']['data'] as $domain) {
+			$results[] = $domain['domain'];
+		}
+		return $results;
+	}
+	
+	
+	function addPrivacyDomain($domain = null) {
+		if (is_null($domain)) {
+			$domain = $_SERVER['SERVER_NAME'];
+		}
+		$result = Vimeo::getInstance()->request($this->getVimeoUri() . '/privacy/domains/'. urlencode($domain), array(), 'PUT');
+		return $this;
+	}
+	
+	
+	function unlock() {
+		$vimeo = Vimeo::getInstance();
+		// This is no longer needed for albums:
+// 		if ($this->getPrivacyView() != 'anybody') {
+// 			$this->setPrivacyView('anybody');
+// 		}
+		if ($this->getPrivacyEmbed() == 'private') {
+			$this->setPrivacyEmbed('whitelist');
+		}
+		if (!in_array($_SERVER['SERVER_NAME'], $this->getPrivacyDomains())) {
+			$this->addPrivacyDomain();
+		}
+	}
+	
+	
+	static function getAll() {
+		$channels = Channel::getAll();
+		$results = array();
+		foreach ($channels as $channel) {
+			$videos = $channel->getVideos();
+			foreach ($videos as $video) {
+				$results[$video->getId()] = $video;
+			}
+		}
+		return $results;
+	}
+	
+	
+	function setSearchScore($score) {
+		$this->searchScore = $score;
+		return $this;
+	}
+	
+	function getSearchScore() {
+		return $this->searchScore;
+	}
+	
+	
+	
+	static function search($str, $context) {
+		
+		foreach ($context as $video) {
+			$score = 0;
+			
+		}
+		
+		
+		$resposne = Vimeo::getInstance()->request('/me/videos', array('query' => $str, 'per_page' => 50));
+// 		var_dump($resposne);
+		$videosResults = array();
+		if (!empty($resposne['body']['data'])) {
+			foreach ($resposne['body']['data'] as $video) {
+				$videoId = Channel::parseId($video['uri']);
+				$videosResults[$videoId] = $videoId;
+			}
+		}
+		
+		// Filter videos which are associated with context
+		foreach ($videosResults as $videoId => &$video) {
+			if (isset($context[$videoId])) {
+				$video = $context[$videoId];
+			} else {
+				$video = null;
+			}
+		}
+		
+		return array_filter($videosResults);
+		
+	}
+	
+	
+	function getChannelId() {
+		return $this->getChannel()->getId();
+	}
+	
+	
+	
+}
---
+++ model/Vimeo.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace com\cminds\videolesson\model;
+use com\cminds\videolesson\App;
+
+require_once App::path('lib/Vimeo/Vimeo.php');
+
+class Vimeo extends \Vimeo\Vimeo {
+	
+	const TRANSIENT_PREFIX = 'cmvlvimeo';
+	
+	const CACHE_ENABLED = 1;
+	const CACHE_DISABLED = 2;
+	const CACHE_DISABLED_ONCE = 3;
+	
+	static protected $instance;
+	
+	protected $cache = self::CACHE_ENABLED;
+	
+	
+	/**
+	 * Get instance.
+	 * 
+	 * @return \Vimeo\Vimeo
+	 */
+	static function getInstance() {
+		if (empty(self::$instance)) {
+			self::$instance = new self(
+				Settings::getOption(Settings::OPTION_VIMEO_CLIENT_ID),
+				Settings::getOption(Settings::OPTION_VIMEO_CLIENT_SECRET),
+				Settings::getOption(Settings::OPTION_VIMEO_ACCESS_TOKEN)
+			);
+		}
+		return self::$instance;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see \Vimeo\Vimeo::request()
+	 */
+	public function request($url, $params = array(), $method = 'GET', $json_body = true) {
+		$cache = ($this->isCacheEnabled() AND $method == 'GET');
+		if ($cache) {
+			$transient = self::TRANSIENT_PREFIX . '_' . md5(implode('___', array($url, serialize($params), $method, $json_body)));
+			$result = get_transient($transient);
+		}
+		if (empty($result) OR empty($result['body'])) {
+			$result = parent::request($url, $params, $method, $json_body);
+			if ($cache) {
+				set_transient($transient, $result, $expiration = Settings::getOption(Settings::OPTION_VIMEO_CACHE_SEC));
+			}
+		}
+		return $result;
+	}
+	
+	
+	static function clearCache() {
+		global $wpdb;
+		$wpdb->query($wpdb->prepare("DELETE FROM $wpdb->options WHERE option_name LIKE %s", '_transient_'. self::TRANSIENT_PREFIX .'_%'));
+	}
+	
+	
+	function isCacheEnabled() {
+		if ($this->cache == self::CACHE_DISABLED_ONCE) {
+			$this->cache = self::CACHE_ENABLED;
+			return false;
+		} else {
+			return ($this->cache == self::CACHE_ENABLED);
+		}
+	}
+	
+	function disableCacheOnce() {
+		$this->cache = self::CACHE_DISABLED_ONCE;
+		return $this;
+	}
+	
+	function disableCache() {
+		$this->cache = self::CACHE_DISABLED;
+		return $this;
+	}
+	
+	
+	function enableCache() {
+		$this->cache = self::CACHE_ENABLED;
+		return $this;
+	}
+	
+
+}
